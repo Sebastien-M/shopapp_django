ac0eedb03f8b3e3f335448e016e51813050d7138
sebastienM <seb.mandaba@gmail.com>
2017-12-05 18:03:35 +0100

    model changed

--- app01/shop/models.py
+++ app01/shop/models.py
@@ -2,19 +2,16 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
-# @python_2_unicode_compatible
-# class Contract(models.Model):
-
-
 @python_2_unicode_compatible
 class Custommer:
-    firstname = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     age = models.IntegerField()
     number = models.CharField(max_length=20)
 
     def __str__(self):
         return self.lastname
+
 
 @python_2_unicode_compatible
 class Period(models.Model):
@@ -26,13 +23,41 @@ class Period(models.Model):
 
 
 @python_2_unicode_compatible
-class Price_Plan(models.Model):
+class PricePlan(models.Model):
     code = models.IntegerField()
 
     def __str__(self):
         return self.code
 
+
 @python_2_unicode_compatible
 class Product(models.Model):
     code = models.IntegerField()
     price = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+@python_2_unicode_compatible
+class Contract(models.Model):
+    owner = models.OneToOneField(
+        Custommer,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    price_plan = models.OneToOneField(
+        PricePlan,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    period = models.OneToOneField(
+        Period,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return self.owner
