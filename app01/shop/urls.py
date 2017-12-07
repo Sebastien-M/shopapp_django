4e1bfe9432c1281cf0715827a61a2a3fe356ab8f
sebastienM <seb.mandaba@gmail.com>
2017-12-07 10:31:32 +0100

    done

--- app01/shop/urls.py
+++ app01/shop/urls.py
@@ -6,4 +6,7 @@ app_name = "shop"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^new$', views.new, name='new'),
+    url(r'^(?P<priceplan_id>[0-9]+)infos/$', views.infos, name='infos'),
+    url(r'^(?P<order_id>[0-9]+)save/$', views.save, name='save'),
+    url(r'^orders$', views.orders, name='orders'),
 ]
--- app01/shop/views.py
+++ app01/shop/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 from django.template import loader
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from .models import PricePlan
+from django.shortcuts import render
+from .models import PricePlan, Order
 
 
 def index(request):
@@ -11,8 +11,31 @@ def index(request):
     context = {}
     return HttpResponse(template.render(context, request))
 
+
 def new(request):
     pps = PricePlan.objects.all()
-
     context = {'priceplans': pps}
     return render(request, 'shop/new.html', context)
+
+
+def infos(request, priceplan_id):
+    priceplan = PricePlan.objects.get(pk=priceplan_id)
+    order = Order(priceplan=priceplan)
+    order.save()
+    context = {'pp': priceplan, 'order': order}
+    return render(request, 'shop/infos.html', context)
+
+
+def save(request, order_id):
+    order = Order.objects.get(pk=order_id)
+    order.custommer_first_name = request.POST['first_name']
+    order.custommer_last_name = request.POST['last_name']
+    order.save()
+    context = {'order': order}
+    return render(request, 'shop/save.html', context)
+
+
+def orders(request):
+    orders = Order.objects.all()
+    context= {'orders': orders}
+    return render(request,'shop/orders.html', context)
